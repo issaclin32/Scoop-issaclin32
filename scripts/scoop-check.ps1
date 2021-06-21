c2da633d02e61af0cbe51ccba4fd93007ebbbd07
Issac Lin <issaclin32@gmail.com>
2021-06-21 22:00:35 +0800

    update 'scoop-check'

--- scripts/scoop-check.ps1
+++ scripts/scoop-check.ps1
@@ -3,5 +3,6 @@ scoop status
 $choice = Read-Host "Update All [Y/N] "
 if ($choice.ToLower() -eq 'y') {
     sudo scoop update *
-    scoop cleanup * --cache
+    scoop cleanup *
+    scoop cache rm *
 }
